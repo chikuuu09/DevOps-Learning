7ddbb21f9c85ce8e40b106b3b1f1d701baacfa99
EC2 Default User <ec2-user@ip-172-31-24-255.ec2.internal>
2026-08-16 15:48:20 +0000

    Add GitHub access audit script

---
+++ Projects/github-access-audit/github_access.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+################################
+# Author      : Suraj
+# Date        : 16/08/2026
+# Version     : v1.0
+#
+# Description:
+# This script uses the GitHub API to audit repository
+# collaborators and identify users who have read access.
+#
+# It takes the repository owner and repository name as
+# command-line arguments and uses curl to fetch
+# collaborator information from GitHub
+#
+# Usage:
+# ./github_access.sh <repo_owner> <repo_name>
+# ################################
+
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
